d46f16b4722728f0d9f333bfd9faf30e2b1faa52
amudha <amudha@ebi.ac.uk>
2022-05-30 16:52:50 +0100

    emicss=false & pfam-ver error fixed

--- EMICSS/EmicssXML.py
+++ EMICSS/EmicssXML.py
@@ -361,7 +361,11 @@ class EmicssXML:
                 if "PFAM" not in all_db:
                     db = EMICSS.dbType()
                     db.set_db_source("%s" % "PFAM")
-                    db.set_db_version("%s" % self.pfam_vers)
+                    try:
+                        if self.pfam_vers:
+                            db.set_db_version("%s" % self.pfam_vers)
+                    except AttributeError as e:
+                        print("Pfam version not collected", e)
                     dbs.add_db(db)
                 all_db.add("PFAM")
 
