c96dc0c00b17572d30e7c8a238c1e351d1f71e41
Neli Fonseca <neli@ebi.ac.uk>
2022-07-29 12:30:10 +0100

    Improved CPX tests

--- unit_test/test_ComplexPortalMapping.py
+++ unit_test/test_ComplexPortalMapping.py
@@ -10,20 +10,18 @@ class TestComplexPortalMapping(unittest.TestCase):
     UnitTest for Complex Portal annotations
     """
 
-    # @patch.object(resources.ComplexPortalMapping.CPX_database, 'cpx_database', return_value='cpx_db')
-    @mock.patch("resources.ComplexPortalMapping.CPX_database.get_from_cpx")
-    @mock.patch("resources.ComplexPortalMapping.CPX_database.get_from_uniprot")
-    def test_worker(self, uniprot_mock, cpx_mock):
-        uniprot_mock.side_effect = [['CPX-4881', 'CPX-4883', 'CPX-4885'], ['CPX-4881', 'CPX-4883', 'CPX-4885'], [],
+    def setUp(self):
+        self.uniprot_side_effect = [['CPX-4881', 'CPX-4883', 'CPX-4885'], ['CPX-4881', 'CPX-4883', 'CPX-4885'], [],
                         ['CPX-4881', 'CPX-4883', 'CPX-4885'], ['CPX-4881', 'CPX-4883', 'CPX-4885'], [],
                         []]
-        row = [['CPX-4881', 'DNA-directed RNA polymerase holoenzyme complex, Sigma70 variant', 'DNA', '83333', 'CHEBI:18420(1)|CHEBI:29105(2)|P00579(1)|P0A7Z4(2)|P0A800(1)|P0A8T7(1)|P0A8V2(1)', 'ECO:0000353(evidence used in manual assertion)', 'intact:EBI-22113905', 'GO:0003677(DNA binding)|GO:0003899(DNA-directed RNA polymerase activity)', 'pubmed:28666008(see-also)|pubmed:30109846(see-also)|wwpdb:3lu0(subset)|complex portal:CPX-4883(complex-primary)|complex portal:CPX-4881(inferred-from)|pubmed:21398637(see-also)|rhea:RHEA:21248(identity)|intenz:2.7.7.6(identity)'],
+        self.row = [['CPX-4881', 'DNA-directed RNA polymerase holoenzyme complex, Sigma70 variant', 'DNA', '83333', 'CHEBI:18420(1)|CHEBI:29105(2)|P00579(1)|P0A7Z4(2)|P0A800(1)|P0A8T7(1)|P0A8V2(1)', 'ECO:0000353(evidence used in manual assertion)', 'intact:EBI-22113905', 'GO:0003677(DNA binding)|GO:0003899(DNA-directed RNA polymerase activity)', 'pubmed:28666008(see-also)|pubmed:30109846(see-also)|wwpdb:3lu0(subset)|complex portal:CPX-4883(complex-primary)|complex portal:CPX-4881(inferred-from)|pubmed:21398637(see-also)|rhea:RHEA:21248(identity)|intenz:2.7.7.6(identity)'],
                ['CPX-4883', 'DNA-directed RNA polymerase holoenzyme complex, SigmaS variant', 'DNA', '83333', 'CHEBI:18420(1)|CHEBI:29105(2)|P0A7Z4(2)|P0A800(1)|P0A8T7(1)|P0A8V2(1)|P13445(1)', 'ECO:0005546(biological system)', 'intact:EBI-22113905', 'GO:2000142(regulation)', 'pubmed:28666008(see-also)|pubmed:30109846(see-also)|wwpdb:3lu0(subset)|complex portal:CPX-4883(complex-primary)|complex portal:CPX-4881(inferred-from)|pubmed:21398637(see-also)|rhea:RHEA:21248(identity)|intenz:2.7.7.6(identity)'],
                ['CPX-4885', 'DNA-directed RNA polymerase holoenzyme complex, SigmaE variant', 'DNA', '83333', 'CHEBI:18420(1)|CHEBI:29105(2)|P0A7Z4(2)|P0A800(1)|P0A8T7(1)|P0A8V2(1)|P0AGB6(1)', 'ECO:0000353(physical interaction evidence)', 'intact:EBI-22113905', 'GO:0090605(submerged biofilm formation)|GO:0036460(cellular response to cell envelope stress)', 'pubmed:28666008(see-also)|pubmed:30109846(see-also)|wwpdb:3lu0(subset)|complex portal:CPX-4883(complex-primary)|complex portal:CPX-4881(inferred-from)|pubmed:21398637(see-also)|rhea:RHEA:21248(identity)|intenz:2.7.7.6(identity)']]
-        cpx_mock.side_effect = [CPX(row[0]), CPX(row[1]), CPX(row[2]),
-                                CPX(row[0]), CPX(row[1]), CPX(row[2]),
-                                CPX(row[0]), CPX(row[1]), CPX(row[2])]
-        proteins = [
+
+        self.cpx_side_effect = [CPX(self.row[0]), CPX(self.row[1]), CPX(self.row[2]),
+                                CPX(self.row[0]), CPX(self.row[1]), CPX(self.row[2]),
+                                CPX(self.row[0]), CPX(self.row[1]), CPX(self.row[2])]
+        self.proteins = [
             set_protein("EMD-0001", "1", "RNA polymerase-sigma54 holoenzyme", "83333", ['6GFW'], ['1', '2'],
                          "P0A7Z4", "UNIPROT",
                          "MQGSVTEFLKPRLVDIEQVSSTHAKVTLEPLERGFGHTLGNALRRILLSSMPGCAVTEVEIDGVLHEYSTKEGVQEDILEILLNLKG"
@@ -41,23 +39,31 @@ class TestComplexPortalMapping(unittest.TestCase):
             set_protein("EMD-0001", "4", "DNA", "83333", "", "" , "P0A7Z4", "UNIPROT",
                          "MARVTVQDAVEKIGNRFDLVLVAARRARQMQVGGKDPLVPEENDKTTVIALREIEEGLINNQILDVRERQEQQEQEAAEL QAVTAIAEGRR",
                          "1", "", "", "", "", "", "", "", "", "")]
-        supras = [set_supra('EMD-0001', 'supra_1', 'RNA polymerase-sigma54 holoenzyme', 'supra'),
+        self.supras = [set_supra('EMD-0001', 'supra_1', 'RNA polymerase-sigma54 holoenzyme', 'supra'),
                   set_supra('EMD-0001', 'supra_2', 'DNA-directed RNA polymerase', 'supra'),
                   set_supra('EMD-0001', 'supra_4', 'DNA', 'supra')]
-        EMDB_complex = [set_EMDB_comp('EMD-0001', 'EMD-0001_1', 'RNA polymerase-sigma54 holoenzyme', '1', '', ['CPX-4881', 'CPX-4883', 'CPX-4885'],
+        self.EMDB_complex = [set_EMDB_comp('EMD-0001', 'EMD-0001_1', 'RNA polymerase-sigma54 holoenzyme', '1', '', ['CPX-4881', 'CPX-4883', 'CPX-4885'],
                                        {'P0A800', 'P0A8T7', 'P0A7Z4'}, 'Complex Portal', ''),
                         set_EMDB_comp('EMD-0001', 'EMD-0001_2', 'DNA-directed RNA polymerase', '1', '', ['CPX-4881', 'CPX-4883', 'CPX-4885'],
                                        {'P0A800', 'P0A8T7', 'P0A7Z4'}, 'Complex Portal', ''),
                         set_EMDB_comp('EMD-0001', 'EMD-0001_4', 'DNA', '1', '',[], {'P0987'}, '', '')]
 
+
+
+    # @patch.object(resources.ComplexPortalMapping.CPX_database, 'cpx_database', return_value='cpx_db')
+    @mock.patch("resources.ComplexPortalMapping.CPX_database.get_from_cpx")
+    @mock.patch("resources.ComplexPortalMapping.CPX_database.get_from_uniprot")
+    def test_worker(self, uniprot_mock, cpx_mock):
+        uniprot_mock.side_effect = self.uniprot_side_effect
+        cpx_mock.side_effect = self.cpx_side_effect
         cpx_list = [('CPX-4881', 'CPX-4883', 'CPX-4885'), ('CPX-4881', 'CPX-4883', 'CPX-4885'), '']
         score = (0.6, 0.6, 0.0)
         provenance = ("Complex Portal", "Complex Portal", "")
-        ComplexMap = resources.ComplexPortalMapping.CPMapping(proteins, supras, "CP_FTP")
+        ComplexMap = resources.ComplexPortalMapping.CPMapping(self.proteins, self.supras, "CP_FTP")
 
-        self.assertEqual(len(proteins), 3)
-        for n in range(len(proteins)-1):
-            EM_comp = ComplexMap.worker(EMDB_complex[n])
+        self.assertEqual(len(self.proteins), 3)
+        for n in range(len(self.proteins)-1):
+            EM_comp = ComplexMap.worker(self.EMDB_complex[n])
             cpx_id_list = ()
             for x in range(len(EM_comp.cpx_list)):
                 cpx_id_list = cpx_id_list + (EM_comp.cpx_list[x].cpx_id,)
@@ -65,4 +71,4 @@ class TestComplexPortalMapping(unittest.TestCase):
             self.assertEqual(EM_comp.score, score[n])
             self.assertEqual(EM_comp.provenance, provenance[n])
 
-        self.assertIsNone(ComplexMap.worker(EMDB_complex[-1]))
+        self.assertIsNone(ComplexMap.worker(self.EMDB_complex[-1]))
